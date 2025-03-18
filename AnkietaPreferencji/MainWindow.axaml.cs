f9e45bc99db63836cd354fbfe7809c1be85d8be7
tomek <gg@pdw.com>
2025-03-18 08:54:39 +0100

    C# Zatwierdzenie wprowadzenia danych

--- AnkietaPreferencji/MainWindow.axaml.cs
+++ AnkietaPreferencji/MainWindow.axaml.cs
@@ -1,5 +1,9 @@
+using System;
+using System.IO;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Tmds.DBus.Protocol;
+using Path = Avalonia.Controls.Shapes.Path;
 
 namespace AnkietaPreferencji;
 
@@ -8,6 +12,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        SubmitButton.Click += SubmitButton_Click;
     }
     
     private string Imie;
@@ -15,7 +20,27 @@ public partial class MainWindow : Window
 
     private void SubmitButton_Click(object? sender, RoutedEventArgs e)
     {
+        
         Imie = imie.Text;
         Zainteresowania = zainteresowania;
+        
+        try
+        {
+            var comboBoxValue = (zainteresowania.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "Nic";
+
+            var content = $"ComboBox: {comboBoxValue}";
+
+            var desktopPath = Environment.GetFolderPath((Environment.SpecialFolder.Desktop));
+            var filePath = System.IO.Path.Combine(desktopPath, "dane z apki");
+            File.WriteAllText(filePath, content);
+
+
+            InfoLabel.Content = "Zapisano";
+        }
+        catch (Exception exception)
+        {
+            InfoLabel.Content = $"Błąd: {exception.Message}";
+
+        }
     }
 }
